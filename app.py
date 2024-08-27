b0d08d5c98f50ceccc756e58716f1d7c6439789a
Wendell Santos <wendell.airam@gmail.com>
2024-08-26 22:48:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -23,3 +23,10 @@ st.title("Prevendo o valor de uma pizza")
 st.divider()
 
 diametro = st.number_input("Digite o diametro da pizza: ")
+preco_previsto = 'Olá, mundo'
+
+if (diametro > 0):
+    preco_previsto = modelo.predict([[diametro]][0][0])
+    st.write(f"O valor da pizza com diamentro {diametro} é de R$ {preco_previsto}")
+else:
+    st.write("Por favor, digite um valor válido")
