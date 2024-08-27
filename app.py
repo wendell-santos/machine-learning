8a539dc52f0aaa35689436050ff1594e700a0a99
Wendell Santos <wendell.airam@gmail.com>
2024-08-26 22:34:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,16 @@
 
 import streamlit as st
 import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+
+#Carregando base de dados
+
+db = pd.read_csv('pizzas.csv')
+
+
+#Treinando modelo
+
+modelo = LinearRegression()
+x = db[["diametro"]]
+y = db[["preco"]]
