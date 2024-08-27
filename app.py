a08501954f68b201ee8ccbb6a59da289ce1ccf1b
Wendell Santos <wendell.airam@gmail.com>
2024-08-26 22:41:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -15,3 +15,11 @@ db = pd.read_csv('pizzas.csv')
 modelo = LinearRegression()
 x = db[["diametro"]]
 y = db[["preco"]]
+
+
+#Criando interface web
+
+st.title("Prevendo o valor de uma pizza")
+st.divider()
+
+diametro = st.number_input("Digite o diametro da pizza: ")
