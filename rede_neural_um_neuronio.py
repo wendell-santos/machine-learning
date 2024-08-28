4ed4708566b7ddbb673608452be952e5426af246
Wendell Santos <wendell.airam@gmail.com>
2024-08-28 19:57:38 -0300

    Update rede_neural_um_neuronio.py

--- rede_neural_um_neuronio.py
+++ rede_neural_um_neuronio.py
@@ -3,12 +3,25 @@ output_desire = 0
 
 input_weight = 0.5
 
+learning_rate = 0.01
+
 def activation(sum):
     if sum >= 0:
         return 1
     else:
         return 0
 
-sum = input * input_weight
+print("entrada", input, "desejado", output_desire)
+while not erro == 0:
+    sum = input * input_weight
 
-output = activation(sum)
+    output = activation(sum)
+
+    print("saida", output)
+
+    erro = output_desire - output
+
+    print(erro)
+
+    if not erro == 0:
+        input_weight = input_weight + learning_rate * input * erro
